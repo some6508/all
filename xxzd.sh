1f7858c1e7e9d45bb2319e50d035b0d9d47e554b
董小豪 <58564994+some6508@users.noreply.github.com>
2020-09-10 21:08:04 +0800

    下载云端数据

---
+++ xxzd.sh
@@ -0,0 +1,9 @@
+
+cd ${0%/*}
+echo "正在下载云端数据"
+curl "http://www.iyuji.cn/iyuji/s/dk92UVdhTnZubGpraG9NL3JJNzZsdz09/1586673855501728" |sed -n 's#.*dxh【</p><p>##p' |sed 's#】dxh.*##; s#</p><p>#\n#g; s#<br/>##g; s#\&nbsp;# #g; s#\&amp;#\&#g; s#\&quot;#\"#g; s/\&lt;/</g; s/\&gt;/>/g' &> ./云端下载.sh
+if [ -f ./云端下载.sh ]; then
+	echo "下载完成"
+else
+	echo "下载失败"
+fi
