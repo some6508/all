256d0092bd9fb53fe5724761817e9c3f4bf982f0
ccaeo <58564994+some6508@users.noreply.github.com>
2021-11-14 15:26:47 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -1,11 +1,24 @@
 
-set -xo pipefail
-PS4='$LINENO: '
-eval `curl -s 'https://api.github.com/repos/changfengoss/pub/commits' | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
+function _CURL() {
+unset url
+echo "Get Link: $@"
+eval `curl -s "https://api.github.com/repos/$@/commits" | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
 eval `curl -s "${url/,}" | sed -n 's/.*"raw_url": /raw_url=/p'`
+}
+
+function CURL() {
+[[ -z $raw_url ]] && return $$
+echo "Download link: $raw_url"
+curl -s -L "${raw_url/,}" -o $1
+unset raw_url
+}
+
+_CURL 'changfengoss/pub'
 if [[ "$raw_url" = *.txt, ]]
-then curl -s -L "${raw_url/,}" -o v
+then CURL v
 elif [[ "$raw_url" = *.yaml, ]]
-then curl -s -L "${raw_url/,}" -o c
-else echo "$url〉$raw_url"
+then CURL c
 fi
+
+_CURL 'pojiezhiyuanjun/freev2'
+CURL c2
