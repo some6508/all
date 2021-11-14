1b7ac3ba9c3b17465bd03e92d6c80ac7bb38e65e
ccaeo <3180965797@qq.com>
2021-11-14 14:15:57 +0800

    202111141415

---
+++ .github/ccaeo.sh
@@ -0,0 +1,10 @@
+
+ls `pwd`/*
+eval `curl -s 'https://api.github.com/repos/changfengoss/pub/commits' | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
+eval `curl -s "${url/,}" | sed -n 's/.*"raw_url": /raw_url=/p'`
+if [[ "$raw_url" = *.txt, ]]
+then curl -s -L "${raw_url/,}" -o v
+elif [[ "$raw_url" = *.yaml, ]]
+then curl -s -L "${raw_url/,}" -o c
+else echo "$url〉$raw_url"
+fi
---
+++ README.md
@@ -0,0 +1,11 @@
+# 每30分钟刷新
+
+## Clash节点订阅
+````
+https://cdn.jsdelivr.net/gh/some6508/all@master/c
+````
+
+## V2ray节点订阅
+````
+https://cdn.jsdelivr.net/gh/some6508/all@master/v
+````
