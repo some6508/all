66b8eb3f9dc857481001548838d403083c739456
ccaeo <58564994+some6508@users.noreply.github.com>
2021-11-20 11:35:42 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -1,9 +1,7 @@
 
-ls -A `pwd`/*
-
 function _CURL() {
 unset url
-echo "Get Link: $@"
+echo "Connection Link: $@"
 eval `curl -s "https://api.github.com/repos/$@/commits" | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
 eval `curl -s "${url/,}" | sed -n 's/.*"raw_url": /raw_url=/p'`
 }
@@ -20,10 +18,12 @@ if [[ "$raw_url" = *.txt, ]]
 then CURL v
 elif [[ "$raw_url" = *.y*ml, ]]
 then CURL c
+else echo "Failed Link: $raw_url"
 fi
 
 _CURL 'pojiezhiyuanjun/freev2'
 if [[ "$raw_url" = *.y*ml, ]]
 then CURL c2
+else echo "Failed Link: $raw_url"
 fi
 sed -i "/^|20/c|$(date '+%Y-%m-%d %H:%M:%S.%N')|" ./README.md
