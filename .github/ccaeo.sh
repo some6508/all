1114045cdf67eeb0cb2f5cdbd377ded1b8771bab
ccaeo <58564994+some6508@users.noreply.github.com>
2021-11-28 11:32:01 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -1,28 +1,28 @@
 
-echo "Start"
+home=`pwd`
+ls -A $home/*
 function _CURL() {
 unset url
-echo "Connection Link: $@"
-eval `curl -s "https://api.github.com/repos/$@/commits" | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
-eval `curl -s "${url/,}" | sed -n 's/.*"raw_url": /raw_url=/p'`
+echo "获取链接: $@"
+eval `curl -sL "https://api.github.com/repos/$@/commits" | sed -n 's/.*"url": /url=/p' | sed -n '3p'`
+eval `curl -sL "${url/,}" | sed -n 's/.*"raw_url": /raw_url=/p'`
 }
 function CURL() {
 [[ -z $raw_url ]] && return $$
-echo "Download link: $raw_url"
-curl -s -L "${raw_url/,}" -o $1
+echo "下载文件: $raw_url"
+curl -sLo $home/$1 "${raw_url/,}"
 unset raw_url
 }
 _CURL 'changfengoss/pub'
-if [[ "$raw_url" = *.txt, ]]
+if [[ "$raw_url" = *.txt* ]]
 then CURL v
-elif [[ "$raw_url" = *.y*ml, ]]
+elif [[ "$raw_url" = *.y*ml* ]]
 then CURL c
-else echo "Failed Link: $raw_url"
+else echo "获取失败: $raw_url"
 fi
 _CURL 'pojiezhiyuanjun/freev2'
-if [[ "$raw_url" = *.y*ml, ]]
+if [[ "$raw_url" = *.y*ml* ]]
 then CURL c2
-else echo "Failed Link: $raw_url"
+else echo "获取失败: $raw_url"
 fi
-sed -i "/^|20/c|$(date '+%Y-%m-%d %H:%M:%S.%N')|" ./README.md
-echo "End"
+sed -i "/^|20/c|$(date '+%Y-%m-%d %H:%M:%S.%N')|" $home/README.md
