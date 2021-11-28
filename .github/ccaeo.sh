6057a07b31a82c936035215b7d9c7f27f5beb86a
ccaeo <58564994+some6508@users.noreply.github.com>
2021-11-29 07:21:04 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -26,11 +26,17 @@ then CURL c2
 else echo "获取失败: $raw_url"
 fi
 }
-eval "`curl -s 'https://github.com/changfengoss/pub/commits' | grep -m 1 'v2rayse comment file' | sed 's|.*href="|href="https://github.com|g; s|">.*|"|g'`"
-eval "`curl -s "$href" | grep -m 1 'data/' | sed 's|.*">|href="https://github.com/changfengoss/pub/raw/main/|g; s|</a>|"|g'`"
-case "$href" in
-*.txt) curl -Lo $home/v "$href";;
-*.yaml) curl -Lo $home/c "$href";;
+eval "`curl -sL 'https://github.com/changfengoss/pub/commits' | grep -m 1 'v2rayse comment file' | sed 's|.*href="|href="https://github.com|g; s|">.*|"|g'`"
+eval "`curl -sL "$href" | grep -m 1 'data/' | sed 's|.*">|href="https://github.com/changfengoss/pub/raw/main/|g; s|</a>|"|g'`"
+case "${href:-ccaeo}" in
+*.txt) curl -sLo $home/v "$href";;
+*.yaml) curl -sLo $home/c "$href";;
+*) echo "未知: $href";;
+esac
+eval "`curl -sL 'https://github.com/pojiezhiyuanjun/freev2/commits' | grep -m 2 'Add files via upload' | sed 's|.*href="|href="https://github.com|g; s|"\>.*|"|g'`"
+eval "`curl -sL "$href" | grep -m 1 '.yml' | sed 's|.*">|href="https://github.com/pojiezhiyuanjun/freev2/raw/master/|g; s|</a>|"|g'`"
+case "${href:-ccaeo}" in
+*.yml) curl -sLo $home/c2 "$href";;
 *) echo "未知: $href";;
 esac
 sed -i "/^|20/c|$(date '+%Y-%m-%d %H:%M:%S.%N')|" $home/README.md
