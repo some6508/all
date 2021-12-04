24b7bb80f082ab587dda711f7091cc9bf5383665
ccaeo <58564994+some6508@users.noreply.github.com>
2021-12-04 14:48:48 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -45,6 +45,6 @@ eval "`curl -sL 'https://github.com/FMYC2015/V2ray/commits' | grep -m 2 'Create 
 #eval "`curl -sL "$href" | grep -m 1 '/blob' | sed 's|.*/blob|href="https://github.com/FMYC2015/V2ray/raw/|g; s|</a>|"|g'`"
 eval "`curl -sL "$href" | sed -n 's|^      <a href="#diff-.*">|href="https://github.com/FMYC2015/V2ray/raw/main/|p' | sed 's|</a>|"|g'`"
 curl -sLo $home/v2 "$href"
-sed -i "/^|20/c|$(date '+%Y年%m月%d日·%H点%M分%S秒.%N毫秒')|" $home/README.md
-sed -i "/^20/c$(date '+%Y年%m月%d日·%H点%M分%S秒.%N毫秒' -d '+6 hour')" $home/README.md
+sed -i "/^|20/c|$(date '+%Y年%m月%d日·%H点%M分%S秒')|" $home/README.md
+sed -i "/^20/c$(date '+%Y年%m月%d日·%H点%M分%S秒' -d '+6 hour')" $home/README.md
 
