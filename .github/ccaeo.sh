f9164e0ad61a6d9643c47599b969555d92d3bb8a
ccaeo <58564994+some6508@users.noreply.github.com>
2021-12-08 20:54:45 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -8,19 +8,25 @@ eval "`curl -sL "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="ht
 case "$href" in
 *.txt) curl -sLo $home/v "$href";;
 *.yaml) curl -sLo $home/c "$href";;
+*) echo "$href";;
 esac
 eval "`curl -sL 'https://api.github.com/repos/pojiezhiyuanjun/freev2/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
 eval "`curl -sL "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://github.com/pojiezhiyuanjun/freev2/raw/master/|p' | sed 's|</a>|"|g'`"
 case "$href" in
 *.yml) curl -sLo $home/c2 "$href";;
+*.zip) curl -sLo $home/2.zip "$href"
+unzip -p $home/2.zip '*txt' >$home/v2
+unzip -p $home/2.zip '*.yml' >$home/c2
+rm -rf $home/2.zip
+;;
 *) echo "$href";;
 esac
 eval "`curl -sL 'https://api.github.com/repos/FMYC2015/V2ray/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
 eval "`curl -sL "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://github.com/FMYC2015/V2ray/raw/main/|p' | sed 's|</a>|"|g'`"
-curl -sLo $home/v2 "$href"
+curl -sLo $home/v3 "$href"
 eval "`curl -sL 'https://api.github.com/repos/FMYC2015/Clash/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
 eval "`curl -sL "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://github.com/FMYC2015/Clash/raw/main/|p' | sed 's|</a>|"|g'`"
 curl -sLo $home/c3 "$href"
-curl -sLo $home/v3 'https://gitee.com/chenx58/clash/raw/V2R/V2R'
+curl -sLo $home/v4 'https://gitee.com/chenx58/clash/raw/V2R/V2R'
 sed -i "/^|20/c|$(date '+%Y年%m月%d日·%H点%M分%S秒')|" $home/README.md
 sed -i "/^20/c$(date '+%Y年%m月%d日·%H点%M分%S秒' -d '+1 hour')" $home/README.md
