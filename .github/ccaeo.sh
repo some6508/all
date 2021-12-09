b8d0d35dbb6d24580202c23f6b0bd5c70082bb01
ccaeo <58564994+some6508@users.noreply.github.com>
2021-12-09 23:57:34 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -14,6 +14,11 @@ eval "`curl -sL 'https://api.github.com/repos/pojiezhiyuanjun/freev2/commits' | 
 eval "`curl -sL "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://github.com/pojiezhiyuanjun/freev2/raw/master/|p' | sed 's|</a>|"|g'`"
 case "$href" in
 *.yml) curl -sLo $home/c2 "$href";;
+*.zip) curl -sLo $home/2.zip "$href"
+unzip -p $home/2.zip '*txt' >$home/v4
+unzip -p $home/2.zip '*.yml' >$home/c2
+rm -rf $home/2.zip
+;;
 *) echo "$href";;
 esac
 eval "`curl -sL 'https://api.github.com/repos/FMYC2015/V2ray/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
