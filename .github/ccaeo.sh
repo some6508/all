c78c35c2e74257fd8666c1557ad2266ab20e3324
ccaeo <58564994+some6508@users.noreply.github.com>
2021-12-13 21:26:41 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -1,6 +1,6 @@
 home=`pwd`
-curl --version
 echo $(date '+%Y年%m月%d日·%H点%M分%S秒.%N毫秒')
+curl --version
 eval "`curl -s 'https://api.github.com/repos/changfengoss/pub/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
 eval "`curl -s "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://raw.githubusercontent.com/changfengoss/pub/main/|p' | sed 's|</a>|"|g'`"
 case "$href" in
