725b983732c5d0864724135d39955fd702383c00
ccaeo <58564994+some6508@users.noreply.github.com>
2021-12-19 18:26:13 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -4,12 +4,12 @@ set -x
 PS4='$LINENO: '
 cd $home
 CURL() {
+echo --------------------------------------------------
 echo "- 下载文件: $1"
 echo "- 连接链接: $2"
 curl -so $home/$1 "$2"
 local CURL=$?
 [[ $CURL = 0 ]] && echo "- 下载完成: $CURL" || echo "! 下载失败: $CURL"
-echo --------------------------------------------------
 }
 echo $(date '+%Y年%m月%d日·周%u·%H点%M分%S秒.%3N毫秒')
 eval "`curl -s 'https://api.github.com/repos/changfengoss/pub/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
@@ -56,4 +56,5 @@ CURL cv 'https://api.v1.mk/sub?target=clash&url=https://raw.githubusercontent.co
 CURL vc 'https://api.v1.mk/sub?target=v2ray&url=https://raw.githubusercontent.com/some6508/all/master/c|https://raw.githubusercontent.com/some6508/all/master/c2|https://raw.githubusercontent.com/some6508/all/master/c3|https://raw.githubusercontent.com/some6508/all/master/c4|https://raw.githubusercontent.com/some6508/all/master/c5|https://raw.githubusercontent.com/some6508/all/master/c6|https://raw.githubusercontent.com/some6508/all/master/v|https://raw.githubusercontent.com/some6508/all/master/v2|https://raw.githubusercontent.com/some6508/all/master/v3|https://raw.githubusercontent.com/some6508/all/master/v4|https://raw.githubusercontent.com/some6508/all/master/v5|https://raw.githubusercontent.com/some6508/all/master/v6|https://raw.githubusercontent.com/some6508/all/master/v7&insert=false&config=https://raw.githubusercontent.com/Meilieage/webcdn/main/rule/Area_Media_NoAuto.ini&exclude=中国&emoji=false&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=true&sort=true&new_name=true'
 sed -i "/^|20/c|$(date '+%Y年%m月%d日·周%u·%H点%M分%S秒.%3N毫秒')|" $home/README.md
 sed -i "/^20/c$(date '+%Y年%m月%d日·周%u·%H点%M分%S秒.%3N毫秒' -d '+1 hour')" $home/README.md
+echo --------------------------------------------------
 echo $(date '+%Y年%m月%d日·周%u·%H点%M分%S秒.%3N毫秒')
