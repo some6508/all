147fa97d5453a9d929559d8dc6aab1e27e4fffc1
some6508 <58564994+some6508@users.noreply.github.com>
2022-02-08 01:38:51 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -17,7 +17,6 @@ return $CURL
 }
 echo $(date '+%Y年%m月%d日·周%u·%H点%M分%S秒.%3N毫秒')
 setsid ping -c 1 -w 1 -A -q some6508.ccaeo.com 
-setsid ping -c 1 -w 1 -A -q ccaeoo.herokuapp.com
 eval "`curl -s 'https://api.github.com/repos/changfengoss/pub/commits' | grep -m 1 'html_url' | sed 's/.*"html_url": /html_url=/g'`"
 eval "`curl -s "${html_url//,}" | sed -n 's|^      <a href="#diff-.*">|href="https://raw.githubusercontent.com/changfengoss/pub/main/|p' | sed 's|</a>|"|g'`"
 case "$href" in
