bde64e1e0a3f84aef3d5c0020b95163bcc02a8b4
some6508 <58564994+some6508@users.noreply.github.com>
2022-05-24 20:19:02 +0800

    Update ccaeo.sh

--- .github/ccaeo.sh
+++ .github/ccaeo.sh
@@ -91,9 +91,9 @@ case "$href" in
 *) ec "! 链接错误	$href" ;;
 esac
 a="`cat $home/url`"
-CURL tmp_clash "https://sub.feng666.tk/sub?target=clash&url=$a&insert=false&config=https://raw.githubusercontent.com/some6508/all/master/fengguo-sjgz.ini&emoji=false&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&sort=false"
-CURL tmp_v2ray "https://sub.feng666.tk/sub?target=v2ray&url=$a&insert=false&config=https://raw.githubusercontent.com/some6508/all/master/fengguo-sjgz.ini&emoji=false&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&sort=false"
-CURL tmp_surfboard "https://sub.feng666.tk/sub?target=surfboard&url=$a&insert=false&config=https://raw.githubusercontent.com/some6508/all/master/fengguo-sjgz.ini&emoji=false&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&sort=false"
+for i in clash v2ray surfboard 
+do CURL tmp_$i "https://api.v1.mk/sub?target=$i&url=$aconfig=https://raw.githubusercontent.com/some6508/all/master/fengguo-sjgz.ini"
+done
 if ! grep -q '</html>' $home/tmp_clash
 then 
 mv -f $home/tmp_clash $home/clash
